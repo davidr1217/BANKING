31619c15190d5f4c0b4146c3d6c1732a3e405218
davidr1217 <davidrivas1217@gmail.com>
2025-10-03 17:17:22 -0500

    Country templates for UI

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,19 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Country
+from .forms import CountryForm
+def country_list(request):
+    countries = Country.objects.all().order_by('name')
+    return render(request, 'authentication_app/country_list.html', {'countries': countries})  # ← CAMBIADO
 
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("This is my first view !!!")
+def country_edit(request, pk):
+    country = get_object_or_404(Country, pk=pk)
+    if request.method == 'POST':
+        form = CountryForm(request.POST, instance=country)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'País actualizado correctamente.')
+            return redirect('authentication_app:country_list')
+    else:
+        form = CountryForm(instance=country)
+    return render(request, 'authentication_app/country_form.html', {'form': form, 'country': country})
