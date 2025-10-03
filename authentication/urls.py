d96fef5b9bd6bf06e92ed8246b0ef157961f5f6c
davidr1217 <davidrivas1217@gmail.com>
2025-10-03 17:14:57 -0500

    Country templates for UI

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'authentication_app'  
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('countries/', views.country_list, name='country_list'),
+    path('countries/<int:pk>/edit/', views.country_edit, name='country_edit'),
 ]
